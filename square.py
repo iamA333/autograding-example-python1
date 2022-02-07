fe9cdb3b878785d51d904f5b76075cf48d26e09d
Abhishek S <67963799+iamA333@users.noreply.github.com>
2022-02-07 10:26:09 +0530

    line 3 and 5

--- square.py
+++ square.py
@@ -1,6 +1,6 @@
 def squaring_num():
     userNum = 3
     userNumSquared = userNum + userNum # Bug here; fix it when instructed
-    print(userNumSquared, end=' ') # Output formatting issue here; fix it when instructed
+    print(userNumSquared) # Output formatting issue here; fix it when instructed
     return userNumSquared
 
