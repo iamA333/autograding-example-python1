4d0423d438ccf5ecc36d1d2a2569fca639a9653a
Abhishek S <67963799+iamA333@users.noreply.github.com>
2022-02-07 10:15:03 +0530

    Changes in line 3 and 5

--- square.py
+++ square.py
@@ -1,2 +1,6 @@
 def squaring_num():
-    return "Hello!"
+    userNum = 3
+    userNumSquared = userNum + userNum # Bug here; fix it when instructed
+    print(userNumSquared, end=' ') # Output formatting issue here; fix it when instructed
+return userNumSquared
+
